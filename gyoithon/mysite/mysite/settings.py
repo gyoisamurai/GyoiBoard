bbb485fbe7ef05f5932b32017b831f2f09dfee12
itakaesu <>
2021-03-22 12:21:21 +0900

    Update.

--- gyoithon/mysite/mysite/settings.py
+++ gyoithon/mysite/mysite/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#so&dkqmtwis&-q#)veznkj@2+ciby_j-_xa!(4t!^)fbru-r9'
+SECRET_KEY = 'DUMMY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
